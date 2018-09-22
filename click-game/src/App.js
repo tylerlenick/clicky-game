8efadac902366b8382271d778bc48b4b4a55a5e1
tylerlenick <tylerlenick@gmail.com>
2018-09-21 20:17:03 -0400

    Added layout and onclicks

--- click-game/src/App.js
+++ click-game/src/App.js
@@ -1,21 +1,102 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from "./components/Nav";
+import CharacterCard from "./components/CharacterCard";
+import characters from "./characters.json";
+import Container from "./Container";
+import Row from "./Row";
+import Column from "./Column";
+import Wrapper from "./components/Wrapper";
+
+function shuffleCharacters(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 
 class App extends Component {
+  state = {
+    characters,
+    currentScore: 0,
+    topScore: 0,
+    rightWrong: "",
+    clicked: [],
+  };
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      rightWrong: ""
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    }
+    else if (newScore === 12) {
+      this.setState({ rightWrong: "You win!" });
+    }
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      rightWrong: "Glaven!",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledCharacters = shuffleCharacters(characters);
+    this.setState({ characters: shuffledCharacters });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+        <Nav
+
+        />
+
+        <Container>
+          <Row>
+            {this.state.characters.map(character => (
+              <Column size="md-3 sm-6">
+                <CharacterCard
+                  key={character.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={character.id}
+                  image={character.image}
+                />
+              </Column>
+            ))}
+          </Row>
+        </Container>
+      </Wrapper>
     );
   }
+}
+
+const randomizeCharacters = (characters) => {
+
+  return characters;
 }
 
 export default App;
